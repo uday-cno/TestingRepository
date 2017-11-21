f59140b252acbc2887161aff74b2719b757540a3
gituser <gituserid@gmail.com>
2017-11-21 12:20:15 +0530

    test

--- Mac2Win/src/reflections/ReflectionsTwo.java
+++ Mac2Win/src/reflections/ReflectionsTwo.java
@@ -1,28 +1,26 @@
 package reflections;
 
-import java.lang.reflect.Method;
-
 public class ReflectionsTwo {
 
 	public static void main(String[] args) throws IllegalAccessException, Exception, Exception{
-		// TODO Auto-generated method stub
+		/*// TODO Auto-generated method stub
 		
 		Class class1 = MainClass.class;
 		MainClass m = new MainClass();
 		
 		// For calling a specific method:
-		/*Method method = class1.getDeclaredMethod("setUp", null);
-		method.invoke(m, null);*/
+		Method method = class1.getDeclaredMethod("setUp", null);
+		method.invoke(m, null);
 		//For calling All methods:
 		Method[] methods = class1.getDeclaredMethods();
 		for (Method method : methods) {
 			System.out.println(method);
 			method.invoke(m, null);
-			
-			
+		*/	
+		System.out.println("hello");	
 		}
 		 
 
-	}}
+	}
 
 
